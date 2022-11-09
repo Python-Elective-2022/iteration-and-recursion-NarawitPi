3d7c97afa59284913c2925b93b39d9c269e95b23
Naravit Piboolvong <tonklar.p@gmail.com>
2022-11-09 08:29:45 +0700

    complete

--- tonklar-1101-iter_rec.py
+++ tonklar-1101-iter_rec.py
@@ -1,3 +1,21 @@
+"""
+function iterativePower
+    parameter: base -> base number
+               exp  -> number of the power
+    result = 1
+    loop exp time:
+        result = result * base
+    :return result
+function recursivePower
+    parameter: base -> base number
+               exp  -> number of the power
+    if power is 0:
+        result = 1
+    else:
+        result = base * recursivePower
+    :return result
+"""
+
 def iterativePower(base, exp):
     result = 1
     for time in range(int(exp)):
@@ -6,15 +24,13 @@ def iterativePower(base, exp):
     return round(result, 2)
 
 def recursivePower(base, exp):
-    result = 1
-    if exp > 0:
-        if exp == 1:
-            result = base
-        else:
-            result = base * recursivePower(base, int(exp) - 1)
+    if exp == 0:
+        result = 1
+    else:
+        result = base * recursivePower(base, int(exp) - 1)
     return round(result, 2)
 
-
+# test case
 for i in range(0,5):
     n = iterativePower(3.3, i)
     print("n", n)
